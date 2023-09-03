89940e70893e6bfcfebc8b52b9d7843cc3e2f05d
unknown <noahbeshahb@gmail.com>
2023-09-03 10:16:49 -0700

    'new-star'

--- containers/Proficiency.tsx
+++ containers/Proficiency.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { SkillBars } from '../portfolio';
 import { Container, Row, Progress, Col } from 'reactstrap';
 import Fade from 'react-reveal/Fade';
 import GreetingLottie from '../components/DisplayLottie';
 
 const Proficiency = () => {
   return (
-    SkillBars && (
+      (
       <Container className="section section-lg">
         <Fade bottom duration={2000}>
           <Row>
